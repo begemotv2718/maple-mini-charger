ac71b6a18ad9c5c3319b392349a2b8fd9c373ed6
Yury <Yury_Adamov@epam.com>
2017-07-16 23:18:49 +0300

    Somewhat working ADC

--- src/maplepwm.c
+++ src/maplepwm.c
@@ -20,6 +20,14 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/timer.h>
+#include <libopencm3/stm32/adc.h>
+#include <libopencm3/cm3/nvic.h>
+
+volatile uint32_t accum=0;
+volatile uint32_t count=0;
+volatile uint32_t count2=0;
+#define ACCUM_VALUE 100
+volatile uint16_t chans[4];
 
 
 static void clock_setup(void)
@@ -33,6 +41,8 @@ static void clock_setup(void)
 	rcc_periph_clock_enable(RCC_GPIOA);
 	rcc_periph_clock_enable(RCC_GPIOB);
 	rcc_periph_clock_enable(RCC_AFIO);
+    rcc_periph_clock_enable(RCC_TIM2);
+    rcc_periph_clock_enable(RCC_ADC1);
 }
 
 static void gpio_setup(void)
@@ -46,6 +56,33 @@ static void gpio_setup(void)
 		      GPIO_TIM4_CH1 | GPIO_TIM4_CH2);
 
 
+    gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ,GPIO_CNF_OUTPUT_PUSHPULL,GPIO0);
+}
+
+void adc1_2_isr(void){
+    int i;
+    for(i=1;i<=4;i++){
+       chans[i-1]=adc_read_injected(ADC1,i);
+    }
+    accum+=chans[0];
+   // TIM4_CCR2=400;
+    count++;
+    if(count>=ACCUM_VALUE){
+      TIM4_CCR1=300;
+      TIM4_CCR2=accum/ACCUM_VALUE;
+      count=0;
+      accum=0;
+      count2++;
+      if(count2>50000){
+        count2=0;
+      }
+    }
+
+
+  
+    gpio_toggle(GPIOB,GPIO0);
+
+    ADC_SR(ADC1) &= ~ADC_SR_JEOC; // Clear JEOC flag
 }
 
 static void tim_setup(void)
@@ -53,7 +90,7 @@ static void tim_setup(void)
 	/* Clock division and mode */
 	TIM4_CR1 = TIM_CR1_CKD_CK_INT | TIM_CR1_CMS_EDGE;
 	/* Period */
-	TIM4_ARR = 65535;
+	TIM4_ARR = 4095;
 	/* Prescaler */
 	TIM4_PSC = 0;
 	TIM4_EGR = TIM_EGR_UG;
@@ -88,6 +125,53 @@ static void tim_setup(void)
 	TIM4_CR1 |= TIM_CR1_CEN;
 }
 
+static void adc_tim_setup(void){
+	/* Clock division and mode */
+	TIM2_CR1 = TIM_CR1_CKD_CK_INT_MUL_4 | TIM_CR1_CMS_EDGE | TIM_CR1_DIR_UP;
+	/* Period */
+    TIM2_ARR=65531;
+    TIM2_PSC=0;
+    //TIM2_EGR = TIM2_EGR_UG;
+    TIM2_CR2 |=TIM_CR2_MMS_UPDATE;
+    TIM2_CR1 |=TIM_CR1_CEN;
+}
+
+static void adc_setup(void){
+
+
+    nvic_set_priority(NVIC_ADC1_2_IRQ, 0);
+    nvic_enable_irq(NVIC_ADC1_2_IRQ);
+
+    gpio_set_mode(GPIOA,GPIO_MODE_INPUT,GPIO_CNF_INPUT_ANALOG,GPIO0);
+    gpio_set_mode(GPIOA,GPIO_MODE_INPUT,GPIO_CNF_INPUT_ANALOG,GPIO1);
+    gpio_set_mode(GPIOA,GPIO_MODE_INPUT,GPIO_CNF_INPUT_ANALOG,GPIO2);
+    gpio_set_mode(GPIOA,GPIO_MODE_INPUT,GPIO_CNF_INPUT_ANALOG,GPIO3);
+
+    adc_enable_scan_mode(ADC1);
+    adc_set_single_conversion_mode(ADC1);
+
+    adc_enable_external_trigger_injected(ADC1,ADC_CR2_JEXTSEL_TIM2_TRGO);
+    adc_set_right_aligned(ADC1);
+    adc_set_sample_time_on_all_channels(ADC1, ADC_SMPR_SMP_55DOT5CYC);
+    uint8_t channels[16];
+    channels[0]=ADC_CHANNEL0;
+    channels[1]=ADC_CHANNEL1;
+    channels[2]=ADC_CHANNEL2;
+    channels[3]=ADC_CHANNEL3;
+    adc_set_injected_sequence(ADC1, 4, channels);
+
+    adc_power_on(ADC1);
+	/* Wait for ADC starting up. */
+    int i;
+	for (i = 0; i < 800000; i++)    /* Wait a bit. */
+		__asm__("nop");
+    adc_reset_calibration(ADC1);
+    adc_calibration(ADC1);
+
+    //adc_enable_eoc_interrupt(ADC1);
+    adc_enable_eoc_interrupt_injected(ADC1);
+}
+
 int main(void)
 {
 	int i, j0, j1, d0, d1;
@@ -95,29 +179,13 @@ int main(void)
 	clock_setup();
 	gpio_setup();
 	tim_setup();
+    adc_setup();
+    adc_tim_setup();
 
-	j0 = 0;
-	d0 = 1;
-	j1 = 128;
-	d1 = 1;
-	while (1) {
-        if(d0>0){
-		   TIM4_CCR1 = 500;
-        }else{
-           TIM4_CCR1 = 100;
-        }
-		j0 += d0;
-		if (j0 == 255)
-			d0 = -1;
-		if (j0 == 0)
-			d0 = 1;
-		TIM4_CCR2 = 600;
-		j1 += d1;
-		if (j1 == 255)
-			d1 = -1;
-		if (j1 == 0)
-			d1 = 1;
-	}
+    TIM4_CCR1 = 10000;
+	TIM4_CCR2 = 32767;
+    
+    while(1);
 
 
 	return 0;
